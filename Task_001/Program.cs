1ef8054541f45156d4e68ed1b6ec8e053673c66f
NukeTree3 <nuketree3@gmail.com>
2023-12-18 22:41:54 +0300

    Создание коммита

---
+++ Task_001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+int input()
+{
+    Console.Write("Введите значение: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void prom(int M, int N)
+{
+    if (M > N) return;
+    Console.Write(M + " ");
+    prom(M + 1, N);
+}
+
+int M = input();
+int N = input();
+prom(M, N);
---
+++ Task_003/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+Random rand = new Random();
+int[] array = new int[rand.Next(1, 100)];
+
+void ArrayCreate(int len)
+{
+    if (0 > len) return;
+    array[len] = rand.Next(1, 100);
+    ArrayCreate(len - 1);
+}
+
+void prom(int N, int[] array)
+{
+    if (0 > N) return;
+    Console.Write(array[N] + " ");
+    prom(N - 1, array);
+}
+
+ArrayCreate(array.Length-1);
+prom(array.Length - 1, array);
